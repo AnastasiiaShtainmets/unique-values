f048355e435d1c2a5ee24937c7f89c9d0176ca86
AnastasiiaShtainmets <nastya589sht@gmail.com>
2022-12-23 10:47:21 +0300

    unique values

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static int[] removeDuplicates(int[] arr) {
+        return Arrays.stream(arr).distinct().toArray();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите длину массива ");
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Введите элементы массива:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int[] distinct = removeDuplicates(arr);
+        System.out.println(Arrays.toString(distinct));
+    }
+}
